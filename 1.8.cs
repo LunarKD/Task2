a50ab452f679b0ea2a8334ed2d3e89c32e260b29
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-17 22:58:41 +0300

    Add files via upload

---
+++ 1.8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[4];
+            Random rnd = new Random();
+            for(var i = 0; i<4; i++)
+            {
+                numbers[i] = rnd.Next(0,100);
+            }
+            Console.WriteLine($"{numbers[0]} {numbers[1]} {numbers[2]} {numbers[3]}");
+        }
+    }
+}
