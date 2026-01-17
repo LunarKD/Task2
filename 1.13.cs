efb2c1026b7fa53a04b19a2d8ee86f82859d72f2
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-17 23:45:41 +0300

    Add files via upload

---
+++ 1.13.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _1._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выбирите сценарий выполнения a, b или c");
+            char inputScen = Convert.ToChar(Console.ReadLine());
+            Console.Clear();
+            switch (inputScen)
+            {
+                case 'a':
+                    Console.WriteLine($"2кг");
+                    Console.WriteLine($"13 17");
+                    break;
+
+                case 'b':
+                    Console.WriteLine("Введите число a");
+                    int a = checkInput();
+                    Console.WriteLine("Введите число b");
+                    int b = checkInput();
+                    Console.Clear();
+                    Console.WriteLine($"{a} 1");
+                    Console.WriteLine($"19 {b}");
+                    break;
+
+                case 'c':
+                    Console.WriteLine("Введите число x");
+                    int x = checkInput();
+                    Console.WriteLine("Введите число y");
+                    int y = checkInput();
+                    Console.Clear();
+                    Console.WriteLine($"{x} {y}");
+                    Console.WriteLine($"5 {y}");
+                    break;
+            }
+        }
+
+        static public int checkInput()
+        {
+            int x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Int32.TryParse(KeayboardInput, out x))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
