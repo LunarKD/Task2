dea522d539944f3014b17750f25d6f6f73b3c62f
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-17 22:43:55 +0300

    Add files via upload

---
+++ 1.3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int input = checkInput();
+            Console.WriteLine($"Вы ввели чсло: {input}");
+        }
+
+        static public int checkInput()
+        {
+            int x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Int32.TryParse(KeayboardInput, out x))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
