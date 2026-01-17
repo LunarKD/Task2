f95e8a30a9e8847a82e92cdae2ce11d99820e45e
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-17 23:07:54 +0300

    Add files via upload

---
+++ 1.10.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5);
+            Console.WriteLine(10);
+            Console.WriteLine(21);
+        }
+    }
+}
