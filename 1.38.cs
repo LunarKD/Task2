1e6369b6f20eea67796461cb1b1423f748cbd642
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-18 22:14:09 +0300

    Add files via upload

---
+++ 1.38.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1._38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите высоту равнобедренной трапеции:");
+            var h = checkInput(false);
+            Console.WriteLine("Введите большее основание равнобедренной трапеции:");
+            var A = checkInput(false);
+            Console.WriteLine("Введите меньшее основание равнобедренной трапеции:");
+            var B  = checkInput(false);
+            var halfDiff = Math.Abs(A - B) / 2.0;
+            var leg = Math.Sqrt(Math.Pow(h,2) + Math.Pow(halfDiff,2));
+            var P = A + B + 2 * leg;
+            Console.WriteLine($"Периметр равнобедренной трапеции = {P}");
+
+        }
+
+        static public double checkInput(bool allowZero)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || (!allowZero && x <= 0))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
