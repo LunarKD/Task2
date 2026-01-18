b6dbe96eb35b042d1202d800dc60fdf03243333f
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-18 21:59:23 +0300

    Add files via upload

---
+++ 1.35.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1._35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первый катет:");
+            var a = checkInput(false);
+            Console.WriteLine("Введите второй катет:");
+            var b = checkInput(false);
+            double x = Math.Sqrt(a * a + b * b);
+            Console.WriteLine($"Гипотенузу прямоугольного треугольника = {x}");
+
+        }
+
+        static public double checkInput(bool allowZero)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || (!allowZero && x <= 0))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
