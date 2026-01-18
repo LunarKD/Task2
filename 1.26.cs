9968812503f20b38435b89c295ada72e87a0878f
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-18 16:15:40 +0300

    Add files via upload

---
+++ 1.26.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1._26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности r:");
+            var r = checkInput();
+            Console.WriteLine($"Диаметр окружности = {2*r}");
+
+
+        }
+
+        static public double checkInput()
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
