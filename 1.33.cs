1ddeed897fed10b77ddd228497fb8e44ae60b355
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-18 17:20:30 +0300

    Add files via upload

---
+++ 1.33.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1._33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество жителей в государстве:");
+            var people = checkInput();
+            Console.WriteLine("Введите площадь территории государства:");
+            var s = checkInput();
+            var p = people / s;
+            Console.WriteLine($"Плотность населения в этом государстве - {p}");
+
+        }
+
+        static public double checkInput()
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
