a569d0d27a19d968a3c2497d0750244e97e2acf1
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-18 16:14:07 +0300

    Add files via upload

---
+++ 1.25.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1._25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите дину стороны квадрат a:");
+            var a = checkInput();
+            Console.WriteLine($"Периметр квадрата = {4*a}");
+
+
+        }
+
+        static public double checkInput()
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
