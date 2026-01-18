858ea0582e5d6a0af7497f35a7205c7282ea51c0
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-18 17:17:08 +0300

    Add files via upload

---
+++ 1.32.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1._32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите объем тела:");
+            var v = checkInput();
+            Console.WriteLine("Введите массу тела:");
+            var m = checkInput();
+            var p = m / v;
+            Console.WriteLine($"Плотность материала этого тела - {p}");
+
+        }
+
+        static public double checkInput()
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
