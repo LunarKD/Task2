bb78a5c650a7f6007df0d0f75a049cfe4b84d4bc
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-18 16:36:00 +0300

    Add files via upload

---
+++ 1.28.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1._28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба a:");
+            var a = checkInput();
+            var v = Math.Pow(a,3);
+            var sSide = 4 * Math.Pow(a,2);
+            Console.WriteLine($"Объем куба - {v},  площадь боковой поверхности куба - {sSide}");
+
+
+        }
+
+        static public double checkInput()
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
