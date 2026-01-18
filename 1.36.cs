9c0a72bef4d1442ad9683ef18885e5ce79834b3a
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-18 22:02:44 +0300

    Add files via upload

---
+++ 1.36.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1._36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите внешний радиус кольца:");
+            var R = checkInput(false);
+            Console.WriteLine("Введите внешний радиус кольца:");
+            var r = checkInput(false);
+            double S = Math.PI * (Math.Pow(R,2) - Math.Pow(r,2));
+            Console.WriteLine($"Площадь кольца = {S}");
+
+        }
+
+        static public double checkInput(bool allowZero)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || (!allowZero && x <= 0))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
