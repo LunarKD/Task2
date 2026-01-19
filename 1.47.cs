2e35766ff60eb005c1a8d2b9cddc1e45df124aaf
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-19 12:07:58 +0300

    Add files via upload

---
+++ 1.47.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _1._47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] points = new double[4];
+           for(var i = 0; i<points.Length;i++)
+           {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine($"Введите x");
+                    points[i] = checkInput(true);
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine($"Введите y");
+                    points[i] = checkInput(true);
+                    Console.Clear();
+                }
+           }
+           double d = Math.Sqrt((points[0] - points[2]) * (points[2] - points[0]) + (points[3] - points[1]) * (points[3] - points[1]));
+           Console.WriteLine($"Расстояния между x1y1 и x2y2 = {d}");
+
+        }
+
+        static public double checkInput(bool allowZero)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || (!allowZero && x <= 0))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            Console.Clear();
+            return x;
+        }
+    }
+}
