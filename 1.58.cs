5ad2efd9f8ca8b98760e2b2ca8e11d576702ea19
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-20 23:48:57 +0300

    Add files via upload

---
+++ 1.58.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1._58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c = (450 - 32.0) / 1.8;
+
+            Console.WriteLine($"450 Фаренгейт = {c} Цельсия.");
+
+        }
+
+
+
+    }
+}
