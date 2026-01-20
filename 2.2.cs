ad00e82557f5ae16314fcf0e7b7764a1f1e152ea
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-21 01:40:57 +0300

    Add files via upload

---
+++ 2.2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество кг:");
+            int kg = (int)checkInput(true,false);
+            Console.WriteLine($"Число полных центнеров: {kg/100}");
+            
+
+        }
+        static public double checkInput(bool allowZero, bool cleanConsole)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || (!allowZero && x < 0))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            if (cleanConsole)
+                Console.Clear();
+            return x;
+        }
+
+    }
+}
