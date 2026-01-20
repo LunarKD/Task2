26b5af8c227019a8773db4f2c8cab30a5ceb0c51
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-21 01:39:08 +0300

    Add files via upload

---
+++ 2.1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество см:");
+            int cm = (int)checkInput(true,false);
+            Console.WriteLine($"Число полных метров: {cm/100}");
+            
+
+        }
+        static public double checkInput(bool allowZero, bool cleanConsole)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || (!allowZero && x < 0))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            if (cleanConsole)
+                Console.Clear();
+            return x;
+        }
+
+    }
+}
