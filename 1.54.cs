21d05fb5e7b899aa32566904b3392028229424ba
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-20 23:25:04 +0300

    Add files via upload

---
+++ 1.54.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1._54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите возраст Тани:");
+            var ageTani  = checkInput(false, true);
+            Console.WriteLine("Введите возраст Мити:");
+            var ageMiti = checkInput(false, true);
+
+            var avg = (ageMiti + ageTani) / 2.0;
+
+            Console.WriteLine($"Их средний возраст: {avg}, Таня {(ageTani-avg>0? "старше": "младше")} на {Math.Abs(ageTani - avg)}, " +
+                $"Мити {(ageMiti - avg > 0 ? "старше" : "младше")} на {Math.Abs(ageMiti - avg)}");
+
+        }
+
+        static public double checkInput(bool allowZero,bool cleanConsole)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || (!allowZero && x < 0))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            if(cleanConsole)
+                Console.Clear();
+            return x;
+        }
+
+    }
+}
