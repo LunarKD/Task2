46764891d2dd63d6fb3812ae2ff5bdd792592f0e
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-20 23:57:06 +0300

    Add files via upload

---
+++ 1.59.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1._59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение переменной a:");
+            var a = checkInput(true,false);
+            Console.WriteLine("Введите значение переменной a:");
+            var b = checkInput(true,false);
+            var t = a;
+            a = b;
+            b = t;
+            Console.WriteLine($"a = {a}, b = {b}");
+        }
+
+        static public double checkInput(bool allowZero,bool cleanConsole)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || (!allowZero && x < 0))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            if(cleanConsole)
+                Console.Clear();
+            return x;
+        }
+
+    }
+}
