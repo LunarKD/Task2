4a6033650d9b8548690554ff52077b12487c65c9
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-20 23:45:08 +0300

    Add files via upload

---
+++ 1.57.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1._57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите температуру в C:");
+            var c  = checkInput(true, true);
+
+
+            double f = 1.8 * c + 32.0;
+            double k = c + 273.15;
+
+            Console.WriteLine($"Температура в Цельсия: {c}, температура в Фаренгейта: {f}, температура в Кельвина: {k}.");
+
+        }
+
+        static public double checkInput(bool allowZero,bool cleanConsole)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || (!allowZero && x < 0))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            if(cleanConsole)
+                Console.Clear();
+            return x;
+        }
+
+    }
+}
