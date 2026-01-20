8a14dc3ce86bf297585d4ab82b81ef2ba97af07d
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-20 23:32:32 +0300

    Add files via upload

---
+++ 1.55.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _1._55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите скорость первого автомобиля:");
+            var v1  = checkInput(false, false);
+            Console.WriteLine("Введите скорость второго автомобиля:");
+            var v2 = checkInput(false, false);
+            Console.WriteLine("Введите растояние между автомобилями:");
+            var s = checkInput(false, false);
+
+            double t = s/(v1+v2);
+
+            Console.WriteLine($"Автомобили встретятся через: {t} ч");
+
+        }
+
+        static public double checkInput(bool allowZero,bool cleanConsole)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || (!allowZero && x < 0))
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            if(cleanConsole)
+                Console.Clear();
+            return x;
+        }
+
+    }
+}
