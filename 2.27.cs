95e135bd3d76583a94f00daf2c8d15c2df31da30
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-21 22:33:59 +0300

    Add files via upload

---
+++ 2.27.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2._27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите четырехзначное число (1 - 999):");
+            int n = (int)checkInput(1,999);
+
+            int a = n % 10;
+            int bc = n / 10;
+            int x = a * 100 + bc;
+
+            Console.WriteLine(x);
+        }
+        static public double checkInput(int min, int max)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || x < min || x > max)
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            return x;
+        }
+      
+
+    }
+}
