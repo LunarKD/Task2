fbe63636a4f6b17011f06c879c808609efc99535
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-21 18:00:29 +0300

    Add files via upload

---
+++ 2.12.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _2._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число (100-999):");
+            int n = (int)checkInput(100,999);
+
+            int ones = n / 100;
+            int tens = (n / 10) % 10;
+            int hundreds = n % 10;
+
+            Console.WriteLine($"Число единиц в нем {ones}");
+            Console.WriteLine($"Число десятков в нем {tens}");
+            Console.WriteLine($"Сумму его цифр {ones + tens + hundreds}");
+            Console.WriteLine($"Произведение его цифр {ones * tens * hundreds}");
+        }
+        static public double checkInput(int min, int max)
+        {
+            double x;
+            string KeayboardInput = Console.ReadLine();
+            while (!Double.TryParse(KeayboardInput, out x) || x < min || x > max)
+            {
+                Console.WriteLine("Некоректный ввод!");
+                KeayboardInput = Console.ReadLine();
+            }
+            return x;
+        }
+      
+
+    }
+}
