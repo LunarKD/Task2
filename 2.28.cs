4a4475d1845e4f3eb8cb8c98110a274dcfeb4614
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-21 22:41:27 +0300

    Add files via upload

---
+++ 2.28.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2._28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 546;
+
+            int b = n / 100;
+            int a = (n / 10) % 10;
+            int c = n % 10;
+
+            int x = a * 100 + b * 10 + c;
+
+            Console.WriteLine(x);
+        }
+
+      
+
+    }
+}
