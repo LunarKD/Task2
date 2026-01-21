d40e04a55808e9a08f64cf909258db69f64e3149
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-21 17:02:34 +0300

    Add files via upload

---
+++ 2.5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"За 234 дня прошло: {234/7} - полных недели");
+            
+
+        }
+        
+
+    }
+}
