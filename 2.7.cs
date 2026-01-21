70522207f66e6665043a557e9b1f05bae5ad5ad7
LunarKD <dima.kevorkov.03@mail.ru>
2026-01-21 17:13:35 +0300

    Add files via upload

---
+++ 2.7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = 543, h = 130, a = 130;
+            int count = (w / a) * (h / a);
+            Console.WriteLine($"Можно отрезать {count} квадратов по 130 мм");
+
+
+        }
+        
+
+    }
+}
